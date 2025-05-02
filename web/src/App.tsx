4a9e8139c40887bf787d9738dc4e517afeaf54a5
vish9812 <vish9812@gmail.com>
2025-05-02 19:26:34 +0530

    init ui

---
+++ web/src/App.tsx
@@ -0,0 +1,11 @@
+import type { Component } from "solid-js";
+
+const App: Component = () => {
+  return (
+    <div>
+      <h1 class="text-3xl font-bold underline">Hello World</h1>
+    </div>
+  );
+};
+
+export default App;
